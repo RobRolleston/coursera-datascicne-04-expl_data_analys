56aedb4081df3c01edf59b57ff6d52b464b7864c
RobRolleston <rob.rolleston@xerox.com>
2015-05-16 16:34:52 -0400

    Plots 1-3 OK... maybe let yscale float on #3....

--- Project-2/script_V0512a.R
+++ Project-2/script_V0512a.R
@@ -1,5 +1,6 @@
 #
 require(dplyr)
+require(ggplot2)
 
 if (file.exists("data.codes.RData")) {
   load("data.codes.RData")
@@ -17,35 +18,30 @@ if (file.exists("data.summary.RData")) {
 }
 
 data.summary.dplyr <- tbl_df(data.summary)
+
 ################-1
 #Have total emissions from PM2.5 decreased in the United States from 1999 to 2008? 
 #Using the base plotting system, make a plot showing the total PM2.5 emission from 
 #all sources for each of the years 1999, 2002, 2005, and 2008.
-if (FALSE) {
 summary.allByYear <- data.summary.dplyr %>% 
   group_by(year) %>% summarise(total = sum(Emissions))
 x <- levels(summary.allByYear$year)
 y <- summary.allByYear$total
-barplot(matrix(c(x,y), nrow=2, byrow=TRUE), 
+barplot(y, ylab="Total Emisssion",
         xlab="Year", names.arg=x,
-        ylab="Total Emisssion",
-        main="Total PM2.5 Emisssion per year",
-        axes=TRUE, axis.lty = "solid")
-}
+        main="Total PM2.5 Emisssion per year" )
 
 ################-2
 #Have total emissions from PM2.5 decreased in the Baltimore City, Maryland (fips == "24510") 
 #from 1999 to 2008? Use the base plotting system to make a plot answering this question.
-summary.Baltimore <- data.summary.dplyr %>% 
+summary.BaltimoreByYear <- data.summary.dplyr %>% 
   filter(fips == "24510") %>%
   group_by(year) %>% summarise(total = sum(Emissions))
-x <- levels(summary.Baltimore$year)
-y <- summary.Baltimore$total
-barplot(matrix(c(x,y), nrow=2, byrow=TRUE), 
+x <- levels(summary.BaltimoreByYear$year)
+y <- summary.BaltimoreByYear$total
+barplot(y, ylab="Baltimore Total",
         xlab="Year", names.arg=x,
-        ylab="Total Emisssion",
-        main="Baltimore PM2.5 Emisssion per year",
-        axes=TRUE, axis.lty = "solid")
+        main=" Emisssion per year in Baltimore")
 
 
 ################-3
@@ -53,15 +49,24 @@ barplot(matrix(c(x,y), nrow=2, byrow=TRUE),
 #which of these four sources have seen decreases in emissions from 1999-2008 for Baltimore City? 
 #Which have seen increases in emissions from 1999-2008? 
 #Use the ggplot2 plotting system to make a plot answer this question.
+summary.BaltimoreEachType <- data.summary.dplyr %>% 
+  filter(fips == "24510") %>%
+  group_by(type, year) %>% summarise(total = sum(Emissions))
+p <- ggplot(data=summary.BaltimoreEachType, 
+            aes(x = year,y = total)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~type)
+p
 
 ################-4
-#Across the United States, how have emissions from coal combustion-related sources changed 
-#from 1999-2008?
+#Across the United States, how have emissions from 
+#coal combustion-related sources changed from 1999-2008?
 
 ################-5
 #How have emissions from motor vehicle sources changed from 1999-2008 in Baltimore City?
 
 ################-6
-#Compare emissions from motor vehicle sources in Baltimore City with emissions from motor 
-#vehicle sources in Los Angeles County, California (fips == "06037"). Which city has seen greater 
-#changes over time in motor vehicle emissions?
+#Compare 
+#emissions from motor vehicle sources in Baltimore City (fips == "24510") with 
+#emissions from motor vehicle sources in Los Angeles County, California (fips == "06037"). 
+#Which city has seen greater changes over time in motor vehicle emissions?
